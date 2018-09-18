b49e74040a375b65498fbc365ee45b556718c2c3
James Finkelstein <jfinkelstein9@me.com>
2018-09-18 02:49:35 -0400

    updated animations

--- scripts/action.js
+++ scripts/action.js
@@ -1,59 +1,59 @@
-init();
-
-// default variables
 
 
-function init () {
-    // animation variables
-    const titlePageOne = document.querySelector('.page-one-title-animate');
-    const titleTextPageOne = document.querySelector('.page-one-body-animate');
-
-    const titlePageTwo = document.querySelector('.page-two-title-animate');
-    const titleTextPageTwo = document.querySelector('.page-two-body-animate');
-    const titlePageThree = document.querySelector('.page-three-title-animation');
-    const titleBodyPageThree = document.querySelector('.page-three-title-body-animate');
-    const titleFooterPageThree = document.querySelector('.page-three-title-footer-animate');
-    const titlePageFour = document.querySelector('.page-four-title-animate');
-    const titlePageFourBody = document.querySelector('.page-four-title-body-animation');
-    const titlePageFourFooter = document.querySelector('.page-four-up-animation');
 
 
+
+$(document).ready(function(){
     $('#fullpage').fullpage({
-            anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-        
-            afterLoad: function(anchorLink, index){
-                var loadedSection = $(this);
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+    
+        afterLoad: function(anchorLink, index){
+            var loadedSection = $(this);
 
-                if(index == 1) {
-                    titlePageOne.classList.add('animate-page-title');
-                    titleTextPageOne.classList.add('animate-two');
-                } else if(index == 2) {
-                    titlePageTwo.classList.add('animate-page-title');
-                    titleTextPageTwo.classList.add('animate-two');
-                } else if(index == 3) {
-                    titlePageThree.classList.add('animate-page-title-no-delay');
-                    titleBodyPageThree.classList.add('animate-two-delay');
-                    titleFooterPageThree.classList.add('animate-two');
-                } else if(index == 4) {
-                    titlePageFour.classList.add('animate-two-down');
-                    titlePageFourBody.classList.add('animate-two');
-                    titlePageFourFooter.classList.add('animate-up');
-                }
-
-            },
-            onLeave: function(index, nextIndex, direction) {
-                var leavingSection = $(this);
-
-                if(index == 1 && direction =='down') {
-                    titlePageOne.classList.remove('animate-page-title');
-                    titleTextPageOne.classList.remove('animate-two');
-                } else if(index == 2 && direction == 'down' || direction == 'up') {
-                    titlePageTwo.classList.remove('animate-page-title');
-                    titleTextPageTwo.classList.remove('animate-two');
-                }
+            if(index == 1) {
+                $('.page-one-title-animate').addClass('animate-page-title');
+                $('.page-one-body-animate').addClass('animate-two');
+            } else if(index == 2) {
+                $('.page-two-title-animate').addClass('animate-two-down');
+                $('.page-two-body-animate').addClass('animate-two');
+            } else if(index == 3) {
+                $('.page-three-title-animation').addClass('animate-page-title-no-delay');
+                $('.page-three-title-body-animate').addClass('animate-two-delay');
+                $('.page-three-title-footer-animate').addClass('animate-fadein-delay');
+            } else if(index == 4) {
+                $('.page-four-title-animate').addClass('animate-two-down');
+                $('.page-four-title-body-animation').addClass('animate-two');
+                $('.page-four-up-animation').addClass('animate-up');
             }
+
+        },
+        onLeave: function(index, nextIndex, direction) {
+            var leavingSection = $(this);
+
+            if(index == 1) {
+                $('.page-one-title-animate').removeClass('animate-page-title');
+                $('.page-one-body-animate').removeClass('animate-two');
+            } else if(index == 2) {
+                $('.page-two-title-animate').removeClass('animate-two-down');
+                $('.page-two-body-animate').removeClass('animate-two');
+            } else if(index == 3) {
+                $('.page-three-title-animation').removeClass('animate-page-title-no-delay');
+                $('.page-three-title-body-animate').removeClass('animate-two-delay');
+                $('.page-three-title-footer-animate').removeClass('animate-fadein-delay');
+            } else if(index == 4) {
+                $('.page-four-title-animate').removeClass('animate-two-down');
+                $('.page-four-title-body-animation').removeClass('animate-two');
+                $('.page-four-up-animation').removeClass('animate-up');
+            }
+            
+        }
     });
-};
+
+    //put things here
+});
+    
+
+
 
 
 
