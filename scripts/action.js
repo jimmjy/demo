aa3d753a4358e5073000d420f402ed24f7033ecc
James Finkelstein <jfinkelstein9@me.com>
2018-09-17 23:28:21 -0400

    updated scroll

--- scripts/action.js
+++ scripts/action.js
@@ -4,6 +4,7 @@ init();
 
 
 function init () {
+    $(document).ready(function() { $('#fullpage').fullpage(); });
 const title = document.querySelector('.page-one-title-animate');
 const titleText = document.querySelector('.animate-two');
 const pageFive = document.querySelector('#page-five');
@@ -19,14 +20,6 @@ title.classList.add('animate-page-one-title');
 titleText.classList.add('.animate-two');
 
 
-const menuContact = document.querySelector('.menu-item-contact');
-
-menuContact.addEventListener('click', function (e){
-    e.preventDefault();
-    pageFive.scrollIntoView({
-        behavior: 'smooth'
-    });
-});
 
 };
 
